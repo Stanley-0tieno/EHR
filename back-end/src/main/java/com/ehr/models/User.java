f655548263cfc92f5c50ffc031fa333023b11b40
MurungaOwen <owenhood80@gmail.com>
2025-12-09 13:37:50 +0300

    user model

--- back-end/src/main/java/com/ehr/models/User.java
+++ back-end/src/main/java/com/ehr/models/User.java
@@ -1,9 +1,41 @@
 package main.java.com.ehr.models;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.util.Date;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+
+    private boolean isVerified = false;
 }
